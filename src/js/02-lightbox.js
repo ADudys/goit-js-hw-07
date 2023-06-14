c361ca0ca7395cd2d4812f951fe9ce2b5001bf9e
ADudys <ania.dudys99@gmail.com>
2023-06-14 22:29:49 +0200

    Gallery in task 2 was created

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+list.classList.add("gallery");
+
+const markup = galleryItems
+  .map(
+    (img) =>
+      `<li>
+      <a class="gallery__item" href="${img.original}">
+      <img class="gallery__image" src="${img.preview}"
+      data-source="${img.original}"
+      alt="${img.description}" />
+    </a>
+    
+    </li>`
+  )
+  .join("");
+
+list.insertAdjacentHTML("afterbegin", markup);
